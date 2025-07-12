6d302ed1e0055cd38385a228636c546ae1aaeb22
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-07-12 22:35:19 +0530

    Adds springboot.

---
+++ backend/Algo-Judge/src/main/java/com/algojudge/algojudge/AlgoJudgeApplication.java
@@ -0,0 +1,13 @@
+package com.algojudge.algojudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoJudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoJudgeApplication.class, args);
+	}
+
+}
---
+++ backend/Algo-Judge/src/test/java/com/algojudge/algojudge/AlgoJudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algojudge.algojudge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoJudgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
