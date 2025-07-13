8a60d407b3b806d239200a3bc7714ebbf2a82d5b
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-07-13 23:27:08 +0530

    Updates the DB realtions within the entities

--- backend/Algo-Judge/src/main/java/com/algojudge/algojudge/entity/Testcase.java
+++ backend/Algo-Judge/src/main/java/com/algojudge/algojudge/entity/Testcase.java
@@ -2,6 +2,8 @@ package com.algojudge.algojudge.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "testcases")
 public class Testcase {
@@ -9,9 +11,16 @@ public class Testcase {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
 
-    int questionId;
     int input;
     int output;
+    boolean isSample;
+
+    @ManyToOne
+    @JoinColumn(name = "problem_id", nullable = false)
+    Problem problem;
+
+    LocalDateTime createdOn;
+    LocalDateTime lastUpdatedOn;
 
 
 }
