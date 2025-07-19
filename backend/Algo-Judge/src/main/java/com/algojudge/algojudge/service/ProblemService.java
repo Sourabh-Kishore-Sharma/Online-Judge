885f3c1604ca505cc699f7f13d4337272484a811
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-07-19 13:24:13 +0530

    Updates the naming of repo object

--- backend/Algo-Judge/src/main/java/com/algojudge/algojudge/service/ProblemService.java
+++ backend/Algo-Judge/src/main/java/com/algojudge/algojudge/service/ProblemService.java
@@ -13,8 +13,8 @@ import java.util.Optional;
 public class ProblemService {
     private final ProblemRepository problemRepository;
 
-    public ProblemService(ProblemRepository ProblemRepository) {
-        this.problemRepository = ProblemRepository;
+    public ProblemService(ProblemRepository problemRepository) {
+        this.problemRepository = problemRepository;
     }
 
     public Problem saveProblem(Problem Problem){
