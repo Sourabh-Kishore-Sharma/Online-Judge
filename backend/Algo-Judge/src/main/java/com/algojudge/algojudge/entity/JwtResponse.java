ad7d2d8c42cf5320d8f96fe8a27a966b6c8b23da
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-07-20 22:41:54 +0530

    Adds auth controller and JwtRepsonse entity when returing.

---
+++ backend/Algo-Judge/src/main/java/com/algojudge/algojudge/entity/JwtResponse.java
@@ -0,0 +1,66 @@
+package com.algojudge.algojudge.entity;
+
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer"; // Standard for JWT token type
+    private int id;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    public JwtResponse(String accessToken, int id, String email, String firstName, String lastName) {
+        this.token = accessToken;
+        this.id = id;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
