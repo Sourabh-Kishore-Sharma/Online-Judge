cf913b1d8b6683b938ac56d3ac85deb83bfbc2fd
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-07-20 21:29:41 +0530

    Adds Login and Request entities.

---
+++ backend/Algo-Judge/src/main/java/com/algojudge/algojudge/entity/RegisterRequest.java
@@ -0,0 +1,63 @@
+package com.algojudge.algojudge.entity;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 40, message = "Password must be between 6 and 40 characters.")
+    private String password;
+
+    @NotBlank
+    private String firstName;
+
+    private String middleName;
+
+    @NotBlank
+    private String lastName;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
