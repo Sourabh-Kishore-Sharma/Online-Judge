d0d81e31f8056f3e6d70d1d3a0d1717d3b7338f6
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-07-26 12:02:26 +0530

    Adds DTO package to handle request/response.

---
+++ backend/Algo-Judge/src/main/java/com/algojudge/algojudge/dto/TestcaseRequestDTO.java
@@ -0,0 +1,8 @@
+package com.algojudge.algojudge.dto;
+
+public class TestcaseRequestDTO {
+    private String input;
+    private String output;
+    private boolean isSample;
+    private int problemId;
+}
