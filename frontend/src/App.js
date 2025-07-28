f5caf3394d9beabd2c6ebf6da8660e99848013b6
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-07-28 19:06:31 +0530

    Adds frontend.

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import LogIn from "./components/LogIn";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="parent-container">
+        <LogIn></LogIn>
+      </div>
+    </div>
+  );
+}
+
+export default App;
