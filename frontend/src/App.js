f68e8ceb43b15417044591f6fbf8944ad3dd42a0
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-08-02 13:28:27 +0530

    Adds compiler page and a endpont for the same.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import LogIn from "./components/LogIn";
 import Register from "./components/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Compiler from "./components/Compiler";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +14,14 @@ function App() {
           <Routes>
             <Route path="/" element={<Register />}></Route>
             <Route path="/login" element={<LogIn />}></Route>
+            <Route
+              path="/compiler"
+              element={
+                <ProtectedRoute>
+                  <Compiler />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
         </Router>
       </div>
--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -1,16 +1,51 @@
+import { useState } from "react";
 import "./LogIn.css";
+import { data, useNavigate } from "react-router-dom";
 
 const LogIn = () => {
-  const handleSubmit = (e) => {
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    setError("");
+
+    try {
+      const response = await fetch("http://localhost:8080/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email: mail, password }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        localStorage.setItem("token", data.token);
+        navigate("/compiler");
+      }
+    } catch {
+      setError(data.message || "Opps, something went wrong.");
+    }
   };
 
   return (
     <div className="login-parent">
       <form className="login-form" onSubmit={handleSubmit}>
-        <input type="mail" placeholder="Email Address"></input>
-        <input type="password" placeholder="Password"></input>
+        <input
+          type="mail"
+          placeholder="Email Address"
+          value={mail}
+          onChange={(e) => setMail(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
         <button type="submit">LogIn</button>
+        {error && <div className="error">{error}</div>}
       </form>
     </div>
   );
