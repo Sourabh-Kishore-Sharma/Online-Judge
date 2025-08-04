446da7ef4836d38b7a418c21224f9c6bcdadf03c
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-08-04 14:41:08 +0530

    Adds ability to validate a submission against testcases.

--- backend/Algo-Judge/src/main/java/com/algojudge/algojudge/service/TestcaseService.java
+++ backend/Algo-Judge/src/main/java/com/algojudge/algojudge/service/TestcaseService.java
@@ -1,10 +1,10 @@
 package com.algojudge.algojudge.service;
 
 import com.algojudge.algojudge.entity.Testcase;
+import com.algojudge.algojudge.repository.ProblemRepository;
 import com.algojudge.algojudge.repository.TestcaseRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -13,7 +13,7 @@ import java.util.Optional;
 public class TestcaseService {
     private final TestcaseRepository testcaseRepository;
 
-    public TestcaseService(TestcaseRepository testcaseRepository) {
+    public TestcaseService(TestcaseRepository testcaseRepository, ProblemRepository problemRepository) {
         this.testcaseRepository = testcaseRepository;
     }
 
@@ -31,5 +31,9 @@ public class TestcaseService {
 
     public void deleteTestcaseById(int id) {
         testcaseRepository.deleteById(id);
+    }
+
+    public List<Testcase> getAllTestcases(int problemId){
+        return testcaseRepository.findByProblem_Id(problemId);
     }
 }
