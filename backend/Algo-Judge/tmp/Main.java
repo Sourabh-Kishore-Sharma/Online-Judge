0154944714bfcc4e10628c9f5f6ecf5c9c781a7d
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-08-07 22:21:46 +0530

    Adds docker files.

--- backend/Algo-Judge/tmp/Main.java
+++ backend/Algo-Judge/tmp/Main.java
@@ -1,5 +1,8 @@
-class Main{
-	public static void main(String[] args){
-		System.out.println("Hello World");
-	}
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        while(true)
+            continue;
+    }
 }
