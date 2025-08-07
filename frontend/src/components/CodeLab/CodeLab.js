db2caf8389e3f1be43e60d77d5eac57268c43dea
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-08-07 08:14:39 +0530

    Adds ability to run/submit code.

--- frontend/src/components/CodeLab/CodeLab.js
+++ frontend/src/components/CodeLab/CodeLab.js
@@ -7,9 +7,90 @@ import Editor from "./Editor";
 const CodeLab = () => {
   const [selectedProblem, setSelectedProblem] = useState(null);
   const [language, setLanguage] = useState("java");
+  const [code, setCode] = useState("");
+  const [output, setOutput] = useState("");
+  const [action, setAction] = useState("");
+  const [input, setInput] = useState("");
+  const [selectedView, setSelectedView] = useState("input");
+
+  const extMap = {
+    cpp: "cpp",
+    java: "java",
+    python: "py",
+  };
+
+  const handleAction = async (type, e) => {
+    e.preventDefault();
+    setOutput("");
+    setAction(type);
+
+    let endpoint = "";
+    if (type === "run") {
+      endpoint = "http://localhost:8080/api/compiler/submit";
+    } else {
+      endpoint = "http://localhost:8080/api/compiler/validate";
+    }
+    setSelectedView("output");
+
+    try {
+      const token = localStorage.getItem("token");
+      const formData = new FormData();
+
+      const ext = extMap[language];
+      const codeBlob = new Blob([code], { type: "text/plain" });
+      formData.append("file", codeBlob, `Main.${ext}`);
+
+      formData.append("language", language);
+      formData.append("input", input);
+      formData.append("problemId", selectedProblem.id);
+
+      const response = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      });
+      const data = await response.json();
+      if (response.ok) {
+        setOutput(data.verdict || "No output received.");
+      } else {
+        setOutput(data.message || "Compilation Failed.");
+      }
+    } catch {
+      setOutput("Network Error.");
+    }
+  };
 
   return (
     <div className="lab-container">
+      <div className="lab-header">
+        <button onClick={() => setSelectedProblem(null)}>All Problems</button>
+        <div className="action-buttons">
+          <button
+            onClick={(e) => handleAction("run", e)}
+            disabled={!selectedProblem || !code}
+          >
+            Run
+          </button>
+          <button
+            onClick={(e) => handleAction("submit", e)}
+            disabled={!selectedProblem || !code}
+          >
+            Submit
+          </button>
+        </div>
+        <select
+          value={language}
+          onChange={(e) => setLanguage(e.target.value)}
+          required
+        >
+          <option value="">Select Language</option>
+          <option value="cpp">C++</option>
+          <option value="python">Python</option>
+          <option value="java">Java</option>
+        </select>
+      </div>
       <div className="navbar-overlay">
         {!selectedProblem && <NavBar onSelectProblem={setSelectedProblem} />}
       </div>
@@ -17,15 +98,43 @@ const CodeLab = () => {
         <div className="lab-item problem-pane">
           {selectedProblem && (
             <div>
-              <button onClick={() => setSelectedProblem(null)}>
-                All Problems
-              </button>
               <Problem problem={selectedProblem} />
             </div>
           )}
         </div>
         <div className="lab-item workspace-pane">
-          <Editor language={language} />
+          <Editor language={language} code={code} setCode={setCode} />
+          <div className="io-toggle-buttons">
+            <button
+              className={selectedView === "input" ? "active" : ""}
+              onClick={() => setSelectedView("input")}
+            >
+              Input
+            </button>
+            <button
+              className={selectedView === "output" ? "active" : ""}
+              onClick={() => setSelectedView("output")}
+            >
+              Output
+            </button>
+          </div>
+          {selectedView === "input" && (
+            <div className="input-window">
+              <textarea
+                placeholder="Enter custom input here..."
+                className="input-textarea"
+                rows={6}
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+              />
+            </div>
+          )}
+
+          {selectedView === "output" && (
+            <div className="output-window">
+              {output || "Program output will appear here"}
+            </div>
+          )}
         </div>
       </div>
     </div>
