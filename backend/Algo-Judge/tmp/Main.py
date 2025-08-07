6782de11cf3436e3cd76f6078ce8cd28af444f4c
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-08-07 22:21:46 +0530

    Adds docker files.

---
+++ backend/Algo-Judge/tmp/Main.py
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read two integers
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // Output their sum
+        System.out.println(a + b);
+    }
+}
