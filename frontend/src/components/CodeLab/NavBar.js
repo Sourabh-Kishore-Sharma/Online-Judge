7db97d4e35b75de0366af36def2c19d9134d6d00
Sourabh-Kishore-Sharma <sourabhsharma1999s@gmail.com>
2025-08-11 09:50:45 +0530

    Adds loading overlay in All problems list.

--- frontend/src/components/CodeLab/NavBar.js
+++ frontend/src/components/CodeLab/NavBar.js
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import "./NavBar.css";
+import LoadingOverlay from "../Overlay/LoadingOverlay";
 
 const NavBar = ({ onSelectProblem }) => {
   const [problems, setProblems] = useState([]);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
   const API_URL = process.env.REACT_APP_API_URL || "";
 
   useEffect(() => {
     const fetchProblems = async () => {
+      setLoading(true);
       try {
         const token = localStorage.getItem("token");
         const response = await fetch(`${API_URL}/api/problems`, {
@@ -20,6 +24,8 @@ const NavBar = ({ onSelectProblem }) => {
         if (response.ok) setProblems(data);
       } catch {
         setError("Opps, something went wrong.");
+      } finally {
+        setLoading(false);
       }
     };
     fetchProblems();
@@ -27,6 +33,7 @@ const NavBar = ({ onSelectProblem }) => {
 
   return (
     <div className="navbar-container">
+      <LoadingOverlay show={loading} />
       <div className="navbar-parent">
         <div className="problems-list">
           {problems.map((problem) => {
